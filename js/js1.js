6c0514119843820f1516d0973408e64de0f8e57c
Dmytro <yakshinex@gmail.com>
2019-06-30 13:12:50 +0300

    3 taska

--- js/js1.js
+++ js/js1.js
@@ -2,35 +2,11 @@
 
 let arr = [1, 5, 1, 3, '4', [3], 'text', 8, '33', 65, -7, {5:3}];
 
-let newArray = [];
-
-for (let i = 0; i < arr.length; i++) {
-  if (typeof arr[i] == 'number') {
-    newArray.push(arr[i]);
-  }
-}
+let newArray = arr.filter(function(number) {
+  return typeof number == "number";
+});
 
 alert (newArray);
-
-*/
-
-/*
-TASK 22222222222222222222222222222222222222222222222222222222222222222
-const arr1 = [1, 2, 3];
-
-let arr2 = [...arr1, ...arr1];
-
-alert(arr2);
-
-*/
-
-/* TASK 3333333333333333333333333333333333333333333333333333333333333333
-
-const arr1 = [1, 2, 3];
-
-let arr2 = [...arr1, ...arr1.reverse()];
-
-alert (arr2);
 
 */
 
@@ -38,15 +14,14 @@ alert (arr2);
 
 const arr = [1, -5, 1, 3, '4', [3], 'text', 8, '33', 65, -7, {5:3}];
 
-var summ = 0;
-
-for (let i = 0; i < arr.length; i++) {
-  if (typeof arr[i] == 'number' && arr[i] > 0) {
-     summ += arr[i];
+var sum = arr.reduce(function(sum, current) {
+  if (typeof current == 'number' && current > 0) {
+     return sum + current;
   }
-}
+  return sum;
+}, 0);
 
-alert (summ);
+alert (sum);
 
 */
 
@@ -54,39 +29,10 @@ alert (summ);
 
 const arr = [1, -5, 1, 3, '4', [3], 'text', 8, '33', 65, -7, {5:3}];
 
-var arr2 = [];
-
-for (let i = 0; i < arr.length; i++) {
-  if (typeof arr[i] == 'number' && arr[i] > 0) {
-     arr2.push(arr[i]);
-  }
-}
+var arr2 = arr.filter(function(number) {
+  return typeof number == "number" && number > 0;
+});
 
 alert (arr2);
 
 */
-
-/* TASK 666666666666666666666666666666666666666666666666666666666666666
-
-const arr = [1, 2, 3, [4], 5, [6, [7, 8], 9]];
-
-let flatArr = arr.flat(Infinity);
-
-alert (flatArr);
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-function isNumeric(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
-}
